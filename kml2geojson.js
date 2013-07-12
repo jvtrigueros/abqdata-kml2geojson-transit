f390dd3b689e02e711297de36462e451957d18e2
Jose V. Trigueros <j.v.trigueros@gmail.com>
2013-07-12 09:23:26 -0600

    Finished cleaning up the kml2geojson.js script

--- kml2geojson.js
+++ kml2geojson.js
@@ -2,73 +2,61 @@
  * Author: Jose V. Trigueros
  */
 var fs = require('fs')
-   ,xml2js = require('xml2js')
-   ,http = require('http')
-   ,request = require('request')
-   ,AWS = require('aws-sdk')
-   ,nconf = require('nconf').file({file:__dirname + '/config.json'})
-//   ,inspect = require('eyes').inspector({maxLength: false})
+  , xml2js = require('xml2js')
 
-var parser = new xml2js.Parser({
-    trim: true,
-    normalize: true,
-    normalizeTags: true,
-    ignoreAttrs: true,
-    explicitRoot: false,
-    explicitArray: false
-})
+var parser = new xml2js.Parser(
+  { trim : true
+  , normalize : true
+  , normalizeTags : true
+  , ignoreAttrs : true
+  , explicitRoot : false
+  , explicitArray : false
+  }
+)
 
-function kml2json(kmlString, callback) {
+exports.kml2geojson = function kml2geojson(kmlString, callback) {
     parser.parseString(kmlString, function(err, result) {
-//        inspect(result)
       try {
-        var routes = result.document.placemark
-        var jsonRoute = []
-        if (routes !== undefined) {
-          routes.forEach(function (route) {
-            var currentRoute = {
-              type : 'Feature',
-              properties: {
-                route      : route.name,
-                heading    : route.style.iconstyle.heading
-              },
-              geometry : {
-                type : 'Point',
-                coordinates: route.point.coordinates.split(',')
-              }
-            }
+        var busRoutes = result.document.placemark
+        var geoJsonRoutes = []
+        if (!busRoutes) {
+          busRoutes.forEach(function (route) {
+            var currentRoute = { type : 'Feature'
+                               , properties : { route : route.name
+                                              , heading : route.style.iconstyle.heading
+                                              }
+                               , geometry : { type : 'Point'
+                                            , coordinates: route.point.coordinates.split(',')
+                                            }
+                               }
+
             // TODO: There is probably a fancier way of doing this mumbo jumbo
             route.description.table.tr.forEach(function (tr) {
               var td = tr.td
               var routeProperties = currentRoute.properties
-              if (td[0].match(/.*(vehicle).*/i)) {
-                routeProperties.vehicle = td[1]
-              } else if (td[0].match(/.*(speed).*/i)) {
-                routeProperties.speed = td[1].split(' ')[0]
-              } else if (td[0].match(/.*(time).*/i)) {
-                routeProperties.timestamp = td[1]
-              } else if (td[0].match(/.*(stop).*/i)) {
-                routeProperties.destination = td[1]
-              }
+              if (td[0].match(/.*(vehicle).*/i)) routeProperties.vehicle = td[1]
+              else if (td[0].match(/.*(speed).*/i)) routeProperties.speed = td[1].split(' ')[0]
+              else if (td[0].match(/.*(time).*/i)) routeProperties.timestamp = td[1]
+              else if (td[0].match(/.*(stop).*/i)) routeProperties.destination = td[1]
             })
 
-            jsonRoute.push(currentRoute)
+            geoJsonRoutes.push(currentRoute)
           })
         }
-        callback(JSON.stringify(jsonRoute,null,0))
+        // Wrapping the header information that completes the GeoJson file
+        geoJsonRoutes = { type : 'FeatureCollection'
+                        , features: geoJsonRoutes
+                        }
+
+        callback(null, geoJsonRoutes)
       } catch (e) {
-        console.log(e)
-        var s3 = new AWS.S3()
-        s3PutObject.Key = 'error_'+ Date.now() +'.xml'
-        s3PutObject.Body = kmlString
-        s3.putObject(s3PutObject)
-        callback({error: e})
+        callback(e, null)
       }
     })
 }
 
 // TODO: Convert this into a test of some sort
 /*fs.readFile(__dirname + '/test/allroutes.kml', function(err, kmlString) {
-  kml2json(kmlString, console.log)
+  kml2geojson(kmlString, console.log)
 })*/
 

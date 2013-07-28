a346cd5b96bff56d1e79e8a2437f62d4bf0cae71
Jose V. Trigueros <j.v.trigueros@gmail.com>
2013-07-28 15:19:39 -0600

    Checkpoint, I don't know what I'm doing, this used to work but now it doesn't

--- s3-uploader.js
+++ s3-uploader.js
@@ -7,37 +7,48 @@ var nconf = require('nconf').file({file: __dirname + '/config.json'})
   , request = require('request')
   , kml2geojson = require('./kml2geojson').kml2geojson
 
-var s3Options = nconf.get('S3Options')
-var s3PutObject = {
-  ACL: s3Options.ACL,
-  Bucket: s3Options.Bucket,
-  ContentType: s3Options.ContentType
-}
 
+// Figure out where the configuration file is, and get the index
 var payloadIdx = -1
 process.argv.forEach( function(val, idx) {
   if ( val == '-payload' )
     payloadIdx = idx + 1
 })
 
-
+// Use that index to read the file, and set the AWS configuration.
 fs.readFile(process.argv[payloadIdx], 'ascii', function(err, payload) {
   var awsKey = JSON.parse(payload)
-  console.log(payload)
-  AWS.config.update({
-    accessKeyId: awsKey.access,
-    secretAccessKey:awsKey.secretaccess,
-    region: awsKey.region
-  })
+
+  var s3 = new AWS.S3(awsKey)
+
 })
 
-request(nconf.get('RouteBaseUrl') + nconf.get('Route'), function(err,res,body) {
+// Read the S3 Options and create JSON object to store these
+var s3Options = nconf.get('S3Options')
+var s3PutOptions = {
+  ACL: s3Options.ACL,
+  Bucket: s3Options.Bucket,
+  ContentType: s3Options.ContentType
+}
+
+var s3 = new AWS.S3()
+s3PutOptions.Key = 'test.json'
+s3PutOptions.Body = 'test'
+
+
+s3.putObject(s3PutOptions, function(err, data) {
+  if( err )
+    console.log('Didn\'t work:' + err )
+})
+
+
+function upload() {request(nconf.get('RouteBaseUrl') + nconf.get('Route'), function(err,res,body) {
 //  console.log(body)
   kml2geojson(body, function(err,jsonRoute) {
-    var s3 = new AWS.S3()
+    /*var s3 = new AWS.S3()
     s3PutObject.Key = s3Options.Key
-    s3PutObject.Body = jsonRoute
-    console.log()
+    s3PutObject.Body = jsonRoute*/
+    console.log(jsonRoute)
 //    s3.putObject(s3PutObject,function(err,data){
       if(err)
         console.log("Route data could not be save due to: " + err)
@@ -46,3 +57,4 @@ request(nconf.get('RouteBaseUrl') + nconf.get('Route'), function(err,res,body) {
 //    })
   })
 })
+}

9080133682845c58267462d2565a7732832c4043
Jose V. Trigueros <j.v.trigueros@gmail.com>
2013-07-28 15:43:57 -0600

    Got the uploader working :shipit:

--- s3-uploader.js
+++ s3-uploader.js
@@ -18,43 +18,30 @@ process.argv.forEach( function(val, idx) {
 // Use that index to read the file, and set the AWS configuration.
 fs.readFile(process.argv[payloadIdx], 'ascii', function(err, payload) {
   var awsKey = JSON.parse(payload)
-
-  var s3 = new AWS.S3(awsKey)
-
-})
-
-// Read the S3 Options and create JSON object to store these
-var s3Options = nconf.get('S3Options')
-var s3PutOptions = {
-  ACL: s3Options.ACL,
-  Bucket: s3Options.Bucket,
-  ContentType: s3Options.ContentType
-}
-
-var s3 = new AWS.S3()
-s3PutOptions.Key = 'test.json'
-s3PutOptions.Body = 'test'
-
-
-s3.putObject(s3PutOptions, function(err, data) {
-  if( err )
-    console.log('Didn\'t work:' + err )
-})
-
-
-function upload() {request(nconf.get('RouteBaseUrl') + nconf.get('Route'), function(err,res,body) {
-//  console.log(body)
-  kml2geojson(body, function(err,jsonRoute) {
-    /*var s3 = new AWS.S3()
-    s3PutObject.Key = s3Options.Key
-    s3PutObject.Body = jsonRoute*/
-    console.log(jsonRoute)
-//    s3.putObject(s3PutObject,function(err,data){
-      if(err)
-        console.log("Route data could not be save due to: " + err)
-      else
-        console.log("Successfully uploaded route data!")
-//    })
+  AWS.config.update({
+    accessKeyId: awsKey.access,
+    secretAccessKey:awsKey.secretaccess,
+    region: awsKey.region
   })
+
+  // Read the S3 Options and create JSON object to store these
+  var s3Options = nconf.get('S3Options')
+
+  // Upload the file
+  upload(new AWS.S3(), s3Options)
 })
+
+function upload(s3, s3Object) {
+  request(nconf.get('RouteBaseUrl') + nconf.get('Route'), function(err,res,body) {
+    kml2geojson(body, function(err,jsonRoute) {
+      s3Object.Body = JSON.stringify(jsonRoute)
+
+      s3.putObject(s3Object,function(err) {
+        if(err)
+          console.log("Route data could not be save due to: " + err)
+        else
+          console.log("Successfully uploaded route data!")
+      })
+    })
+  })
 }

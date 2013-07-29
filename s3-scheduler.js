0a896dba4b3cab77237289ef34f1499c64cf671f
Jose V. Trigueros <j.v.trigueros@gmail.com>
2013-07-28 20:06:18 -0600

    s3-scheduler working and uploading tasks at every 15s for 5mins

--- s3-scheduler.js
+++ s3-scheduler.js
@@ -1,8 +1,14 @@
 /**
+ * This script will run every 5mins, and the data should be uploaded every 15seconds.
+ * Meaning that there will be (5*60)/15 = 20 tasks
  * Author: Jose V. Trigueros
  */
 var ironioHelper = require('./ironio-payload-config')
   , ironio = require('node-ironio')
+
+var taskIntervalRate = 15 // seconds
+var scheduleIntervalRate = 300 // seconds or 5 mins
+var range = Array.apply(null, {length: scheduleIntervalRate/taskIntervalRate}).map(Number.call, Number)
 
 ironioHelper.loadPayload( function(payload) {
   var body = { payload : JSON.stringify(payload)
@@ -11,8 +17,11 @@ ironioHelper.loadPayload( function(payload) {
 
   var project = ironio(payload.token).projects(payload.project_id)
 
-  project.tasks.queue(body, function(err, res) {
-    console.log(res.tasks.length)
+  range.forEach( function(index) {
+    body.delay = index * taskIntervalRate
+    project.tasks.queue(body, function(err) {
+      if( err ) console.log(err)
+    })
   })
 })
 

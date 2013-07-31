cf312a5a0f074a835d5cefa005a036b8e2637e73
Jose V. Trigueros <j.v.trigueros@gmail.com>
2013-07-30 22:24:53 -0600

    Adding option to load payload synchronously

--- ironio-payload-config.js
+++ ironio-payload-config.js
@@ -3,16 +3,25 @@
  */
 var fs = require('fs')
 
-exports.loadPayload = function loadPayload(cb) {
-  // Figure out where the configuration file is, and get the index
+function getPayloadIndex() {
+// Figure out where the configuration file is, and get the index
   var payloadIdx = -1
-  process.argv.forEach( function(val, idx) {
-    if ( val == '-payload' )
+  process.argv.forEach(function (val, idx) {
+    if (val == '-payload')
       payloadIdx = idx + 1
   })
+  return payloadIdx;
+}
 
+exports.loadPayload = function loadPayload(cb) {
+  var payloadIdx = getPayloadIndex();
   fs.readFile(process.argv[payloadIdx], 'ascii', function(err, payload) {
     if( !err )cb(JSON.parse(payload))
     else console.log("Error occurred reading payload file : " + err)
   })
 }
+
+exports.loadPayloadSync = function loadPayloadSync() {
+  return fs.readFileSync(process.argv[getPayloadIndex()], 'ascii')
+}
+
